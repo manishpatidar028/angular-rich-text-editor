e1d8976a4b38907b2c10719cea47ab0757d80d20
Manish Patidar <manishpatidar@Mac-3.lan>
2025-05-26 17:02:36 +0530

    Global license key changes added

--- angular-rich-texteditor/projects/sample-app/src/app/app.component.ts
+++ angular-rich-texteditor/projects/sample-app/src/app/app.component.ts
@@ -1,34 +1,40 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
-import { RichTextEditorComponent, RichTextEditorModule } from 'rich-text-editor';
-import { RTE_TOOLBAR_PRESETS, RTEPreset } from '../../../rich-text-editor/src/lib/rich-text-editor.constant';
+import {
+  RichTextEditorComponent,
+  RichTextEditorModule,
+  RTE_LICENSE_KEY,
+} from 'rich-text-editor';
+import {
+  RTE_TOOLBAR_PRESETS,
+  RTEPreset,
+} from '../../../rich-text-editor/src/lib/rich-text-editor.constant';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, FormsModule, RichTextEditorModule],
-  providers: [],
+  providers: [{ provide: RTE_LICENSE_KEY, useValue: 'your-license-key' }],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   toolbarGroup = RTE_TOOLBAR_PRESETS;
   title = 'sample-app';
-    content = '<p>Initial content goes here</p>';
+  content = '<p>Initial content goes here</p>';
   @ViewChild(RichTextEditorComponent) rte!: RichTextEditorComponent;
 
-    onRteFileUpload(
-  file: File,
-  callback: (url: string|null, err?: string) => void,
-  optionalIndex?: number,
-  optionalFiles?: File[]
-): void {
-  // call your API & pipe the result back
-  console.log('Function hget called on update');
-  
-}
-injectSignature() {
+  onRteFileUpload(
+    file: File,
+    callback: (url: string | null, err?: string) => void,
+    optionalIndex?: number,
+    optionalFiles?: File[]
+  ): void {
+    // call your API & pipe the result back
+    console.log('Function hget called on update');
+  }
+  injectSignature() {
     const html = `<span class="signature">Best regards,<br><strong>Team Growth99</strong></span>`;
     this.rte.insertContentAtCursor(html);
   }
