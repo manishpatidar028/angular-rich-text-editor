c21e13b03843a71d3d0612a7676fd6722f279a8d
Manish Patidar <manishpatidar@Mac.lan>
2025-06-23 13:20:24 +0530

    sample app changes added.

--- angular-rich-texteditor/projects/sample-app/src/app/rte-modal/rte-modal.component.ts
+++ angular-rich-texteditor/projects/sample-app/src/app/rte-modal/rte-modal.component.ts
@@ -1,13 +1,22 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { RichTextEditorModule, RTE_LICENSE_KEY } from 'angular-rich-text-editor';
+import {
+  RichTextEditorModule,
+  RichTextEditorService,
+  RTE_LICENSE_KEY,
+} from 'angular-rich-text-editor';
 import { SharedRichTextEditorComponent } from '../components/shared-rich-text-editor/shared-rich-text-editor.component';
 
 @Component({
   selector: 'app-rte-modal',
   standalone: true,
-  imports: [CommonModule, FormsModule, RichTextEditorModule, SharedRichTextEditorComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RichTextEditorModule,
+    SharedRichTextEditorComponent,
+  ],
   providers: [{ provide: RTE_LICENSE_KEY, useValue: 'your-license-key' }],
   templateUrl: './rte-modal.component.html',
   styleUrls: ['./rte-modal.component.scss'],
@@ -18,19 +27,21 @@ export class RteModalComponent {
   @Output() save = new EventEmitter<string>();
 
   content = '';
-
+  constructor(private richTextEditor: RichTextEditorService) {}
   ngOnInit() {
     this.content = this.initialContent;
     console.log('RTE Modal initialized with content:', this.content);
   }
 
-  valueChange(){
+  valueChange() {
     console.log('Content changed:', this.content);
   }
 
   injectSignature() {
-    const html = '${Hello}';
-    this.content += html; // simple append, you can enhance using ViewChild if needed
+    const html = '${Dynamic Inline Content}';
+    const formatted = `<span class="rte-variable-token" contenteditable="false">${html}</span>`;
+
+    this.richTextEditor.insertContentAtCursor(formatted);
   }
   injectSignature2() {
     const html = '${HIEEEEEEEEE MANISH}';
